132ace588d2178ad89c3c9153af2ad9541c0e31a
Alysha Huot <alyshajhuot@gmail.com>
2024-01-30 12:31:21 -0700

    add schema file to create items_for_sale table in database

---
+++ db/schema/02_items_for_sale.sql
@@ -0,0 +1,12 @@
+-- Drop and recreate Users table
+
+DROP TABLE IF EXISTS items_for_sale CASCADE;
+
+CREATE TABLE items_for_sale (
+  id SERIAL PRIMARY KEY NOT NULL,
+  seller_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  price_cents INTEGER NOT NULL,
+  description VARCHAR(255),
+  image_url VARCHAR(255)
+);
