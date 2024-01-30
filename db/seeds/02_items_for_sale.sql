1c58b64e396203cfeca63e61322c344244a13975
Alysha Huot <alyshajhuot@gmail.com>
2024-01-30 13:30:50 -0700

    create items seed files and add a couple rows of sample data to each

---
+++ db/seeds/02_items_for_sale.sql
@@ -0,0 +1,5 @@
+-- items_for_sale table seeds
+
+INSERT INTO items_for_sale (title, price_cents, description, seller_id, image_url)
+VALUES ('Used Hricane Ukulele', 7000, 'It comes with a strap, carrier bag, extra strings and a tuner (needs lithium battery). Message me if you are interested.', 1, '../../assets/images/ukulele.png');
+('Used Z175 Zimmerman Grand Piano', 1799500, 'It has got a powerful sound, wonderful range and touch.', 2, '../../assets/images/grand-piano.webp');
