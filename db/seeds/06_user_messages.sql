5ddaaebd569c3360d145b15dba6339770a0442f5
Faelixx <sasha.taylor7@gmail.com>
2024-01-30 15:23:27 -0700

    Feat: Add getAllMessages function to database, update 06_messages to 06_user_messages, add seeds to seeds/06_user_messages.sql

---
+++ db/seeds/06_user_messages.sql
@@ -0,0 +1,5 @@
+-- user_messages table seeds
+
+INSERT INTO user_messages (buyer_id, item_id, content)
+VALUES (1, 2, 'Hello, is this item still available?'),
+(2, 2, 'Are you willing to deliver this to my house? I will throw in an extra twenty bucks if you do! =3');
