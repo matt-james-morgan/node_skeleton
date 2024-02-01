602a3caa34b3245e632648899c5a50ffa92ec8d4
matt-james-morgan <matt.james.morgan97@gmail.com>
2024-02-01 17:37:02 -0500

    added users schema and seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,11 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, username, email) VALUES ('John Doe', 'john_doe', 'john.doe@example.com');
+INSERT INTO users (name, username, email) VALUES ('Jane Smith', 'jane_smith', 'jane.smith@example.com');
+INSERT INTO users (name, username, email) VALUES ('Alice Johnson', 'alice_j', 'alice.j@example.com');
+INSERT INTO users (name, username, email) VALUES ('Bob Anderson', 'bob_a', 'bob.a@example.com');
+INSERT INTO users (name, username, email) VALUES ('Eva Brown', 'eva_b', 'eva.b@example.com');
+INSERT INTO users (name, username, email) VALUES ('Michael Johnson', 'michael_j', 'michael.j@example.com');
+INSERT INTO users (name, username, email) VALUES ('Emily Taylor', 'emily_t', 'emily.t@example.com');
+INSERT INTO users (name, username, email) VALUES ('David Lee', 'david_l', 'david.l@example.com');
+INSERT INTO users (name, username, email) VALUES ('Sophia Wilson', 'sophia_w', 'sophia.w@example.com');
+INSERT INTO users (name, username, email) VALUES ('Andrew Miller', 'andrew_m', 'andrew.m@example.com');
