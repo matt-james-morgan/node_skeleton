279753134669fb094fb676b7d8ea99559a8111e6
Faelixx <sasha.taylor7@gmail.com>
2024-02-02 23:09:01 -0700

    Feat: Message window updates in browser using textbox

--- public/scripts/send-message.js
+++ public/scripts/send-message.js
@@ -1,18 +1,25 @@
   const socket = io('http://localhost:8080');
   const messageForm = document.getElementById('send-container');
+  const messageContainer = document.getElementById('received-message');
   const messageInput = document.getElementById('message-input');
 
-  socket.on('chat-message', data => {
-    console.log(data);
-  });
 
-  $(() => {
+// Prevent form data from submitting to server
+    messageForm.addEventListener('submit', e => {
+      e.preventDefault();
+      // Store message text field and send it back to the server
+      const message = messageInput.value;
+      socket.emit('send-chat-message', message);
+      messageInput.value = '';
+    })
 
-  messageForm.addEventListener('submit', e => {
-    e.preventDefault();
-    const message = messageInput.value;
-    socket.emit('send-chat-message', message);
-    messageInput.value = '';
-  })
+    const appendMessage = function (message) {
+      const messageElement = document.createElement('div');
+      messageElement.innerText = message;
+      messageContainer.append(messageElement)
+    }
 
-});
+    socket.on('chat-message', data => {
+      appendMessage(data);
+    });
+
