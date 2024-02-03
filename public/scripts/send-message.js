3456d739269b4001a2213fb272ce881ef1ab7f8d
Faelixx <sasha.taylor7@gmail.com>
2024-02-02 22:46:45 -0700

    Feat: Message submission broadcasts message to all other users connected to app

--- public/scripts/send-message.js
+++ public/scripts/send-message.js
@@ -1,6 +1,18 @@
-
   const socket = io('http://localhost:8080');
+  const messageForm = document.getElementById('send-container');
+  const messageInput = document.getElementById('message-input');
 
   socket.on('chat-message', data => {
     console.log(data);
   });
+
+  $(() => {
+
+  messageForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
+  })
+
+});
--- server.js
+++ server.js
@@ -77,5 +77,7 @@ server.listen(PORT, () => {
 });
 
 io.on('connection', (socket) => {
-  socket.emit('chat-message', 'Hello World')
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', message);
+  })
   });
